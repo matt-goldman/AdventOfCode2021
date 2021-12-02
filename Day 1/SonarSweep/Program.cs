ba1572f0096a77f254c1d7ac5c0b7740743e8398
Matt Goldman <matt.goldman@outlook.com>
2021-12-02 17:32:54 +1100

    days 1 + 2

---
+++ Day 1/SonarSweep/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Sonar Sweep!");
+
+int increaseCount = 0;
+
+var inputFile = File.ReadAllLines("SweepInput.txt");
+
+var inputList = new List<string>(inputFile);
+
+Console.WriteLine($"There are {inputList.Count} input values");
+
+int currentVal = 0;
+
+int currentSum = 0;
+int lastSum = 0;
+
+int valsPushed = 0;
+
+int[] window = new int[3];
+
+foreach (var input in inputList)
+{
+    currentVal = int.Parse(input);
+
+    PushValue(currentVal);
+    
+    if(valsPushed < 2 )
+    {
+        valsPushed++;
+    }
+    else
+    {
+        currentSum = window.Sum();
+    }
+
+    if (lastSum > 0 && currentSum > lastSum)
+    {
+        increaseCount++;
+    }
+
+    //Console.WriteLine($"Current sum: {currentSum}, last sum: {lastSum}, is greater: {currentSum > lastSum}");
+
+    lastSum = currentSum;
+}
+
+Console.WriteLine($"Total increases: {increaseCount}");
+
+void PushValue(int val)
+{
+    window[2] = window[1];
+    window[1] = window[0];
+    window[0] = val;
+}
---
+++ Day 2/Dive/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var inputFile = File.ReadAllLines("Directions.txt");
+
+var inputList = new List<string>(inputFile);
+
+int horizontalPosition = 0;
+
+int aim = 0;
+
+int depth = 0;
+
+foreach (var input in inputList)
+{
+    var vals = input.Split(' ');
+
+    int val = int.Parse(vals[1]);
+
+    switch (vals[0])
+    {
+        case "forward":
+            horizontalPosition += val;
+            depth += (val * aim);
+            break;
+        case "down":
+            aim += val;
+            break;
+        case "up":
+            aim -= val;
+            break;
+    }
+}
+
+Console.WriteLine($"Final position: {horizontalPosition * depth}");
